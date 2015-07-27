8e2bdfedb2faf45e24d9227163039cd59783e3da
Alex <ashum@iastate.edu>
2015-07-27 16:17:24 -0500

    10day

--- R/features.R
+++ R/features.R
@@ -8,18 +8,6 @@ current_hurricane = function(location, key = get_api_key(), raw = FALSE) {
   }
 }
 
-forecast10day = function(location, key = get_api_key(), raw = FALSE) {
-  URL = build_url(key = key, request_type = "forecast", location = location)
-  req = httr::GET(URL)
-  parsed_req = httr::content(req, type = "application/json")
-  if(raw) {
-    return(parsed_req)
-  }
-  
-  stop_for_error(parsed_req)
-  
-}
-
 geolookup = function(location, key = get_api_key()) {
   URL = build_url(key = key, request_type = "geolookup", location = location)
   req = httr::GET(URL)
--- R/forecast.R
+++ R/forecast.R
@@ -61,3 +61,69 @@ forecast3day = function(location,
   
   data.frame(do.call(rbind, df))
 } 
+
+
+
+
+
+forecast10day = function(location, 
+                         date_fmt = "pretty",  ## TODO 
+                         col_names = "pretty", ## TODO: (orig names)
+                         metric = TRUE,
+                         key = get_api_key(), 
+                         raw = FALSE, 
+                         message = TRUE) {
+  
+  URL = build_url(key = key, request_type = "forecast10day", location = location)
+  req = httr::GET(URL)
+  httr::stop_for_status(req)
+  
+  parsed_req = httr::content(req, type = "application/json")
+  
+  if(message) {
+    print(paste0("Requesting: ", URL))
+  }
+  if(raw) {
+    return(parsed_req)
+  }
+  stop_for_error(parsed_req)
+  
+  if(!("forecast" %in% names(parsed_req))) {
+    stop(paste0("Unable to parse forecast JSON for this location: ", location))
+  }
+  
+  if(metric) {
+    tempCol = "celsius"
+    amtCol = "mm"
+    amtCol2 = "cm"
+    spdCol = "kph"
+  } else {
+    tempCol = "fahrenheit"
+    amtCol = "in"
+    amtCol2 = "in"
+    spdCol = "mph"
+  }
+  
+  fcast = parsed_req$forecast$simpleforecast$forecastday
+  df = lapply(fcast, function(x) {
+    list(date = x$date$pretty,
+         temp_high = as.numeric(x$high[[tempCol]]),
+         temp_low = as.numeric(x$low[[tempCol]]),
+         cond = x$conditions,  ## TODO: multiple conditions?
+         p_precip = x$pop,
+         rain_allday = x$qpf_allday[[amtCol]],
+         rain_day = x$qpf_day[[amtCol]],
+         rain_night = x$qpf_night[[amtCol]],
+         snow_allday = x$snow_allday[[amtCol2]],
+         snow_day = x$snow_day[[amtCol2]],
+         snow_night = x$snow_night[[amtCol2]],
+         max_wind = paste(x$maxwind[[spdCol]], x$maxwind$dir, sep = " "),
+         ave_wind = paste(x$avewind[[spdCol]], x$avewind$dir, sep = " "),
+         max_humid = x$maxhumidity,
+         min_humid = x$minhumidity,
+         ave_humidity = x$avehumidity
+    )
+  })
+  
+  data.frame(do.call(rbind, df))
+}
